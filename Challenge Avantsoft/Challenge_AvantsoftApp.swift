a5c82e8a1b419cc1c0edb39705875033c7a9905d
Victor de Brito Marinho Correa <victordevelopersoftware@gmail.com>
2023-11-27 20:16:13 -0300

    Initial Commit

---
+++ Challenge Avantsoft/Challenge_AvantsoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_AvantsoftApp.swift
+//  Challenge Avantsoft
+//
+//  Created by Victor Brito on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_AvantsoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
